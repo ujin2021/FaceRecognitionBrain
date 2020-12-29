06d1b0575186512cb51cad74edbde2dbc69427c4
ujin2021 <ujin2021@naver.com>
2020-12-29 21:52:28 +0900

    state structure로 받기

--- src/App.js
+++ src/App.js
@@ -77,21 +77,22 @@ class App extends Component {
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className="App">
         <Particles className='particles'
           params={{particlesOptions}} />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
         { 
-          this.state.route === 'home'
+          route === 'home'
           ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
           : ( 
-              this.state.route === 'signin'
+              route === 'signin'
               ? <Signin onRouteChange={this.onRouteChange} />
               : <Register onRouteChange={this.onRouteChange} />
             )
